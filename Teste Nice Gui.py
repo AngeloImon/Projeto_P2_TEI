8b849895e4e30593b734d8137b38a61a87839ef3
Angelo Imon <angeloimon@outlook.com>
2025-05-09 08:55:35 -0300

    Update do teste.

--- Teste Nice Gui.py
+++ Teste Nice Gui.py
@@ -1,6 +1,7 @@
 from nicegui import ui
 
 # Não se esqueçam de instalar o nicegui com o comando: pip install nicegui (no terminal)
+# Depois de instalar: pip install --upgrade nicegui
 
 ui.label("Hello, NiceGUI!").style("font-size: 2em; color: blue;")
 ui.button("Click me!", on_click=lambda: ui.notify("Button clicked!")).style(
@@ -10,5 +11,5 @@ ui.input("Type something:").style("font-size: 1.2em; color: purple;")
 ui.button("Submit", on_click=lambda: ui.notify("Submitted!")).style(
     "font-size: 1.5em; color: red;"
 )
-ui.button("Close", on_click=ui.stop).style("font-size: 1.5em; color: orange;")
+ui.button("Close", on_click=lambda: ui.get_app().shutdown()).style("font-size: 1.5em; color: orange;")
 ui.run()
